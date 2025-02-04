9af541d17c30235e88186a663b8686921d71282c
Andrew Taylor <ataylor370@gatech.edu>
2025-02-04 15:32:16 -0500

    Changed the directory of static file such that it was on the outer project file rather than the inner; Changed the settings for Static file such that it recognied this new directory.

--- CS2340Project1/settings.py
+++ CS2340Project1/settings.py
@@ -118,12 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/'
+
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'CS2340Project1/static',
+    BASE_DIR / "static", "CS2340Project1/../static"
 ]
