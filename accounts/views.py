d307e1b81ba0d52418f04851c4b937b65be94295
ChamonEGT <132160639+ChamonEGT@users.noreply.github.com>
2025-02-13 02:08:28 -0500

    cook

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from .forms import CustomUserCreationForm,CustomErrorList
 from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
 @login_required
 def logout(request):
     auth_logout(request)
@@ -44,4 +45,12 @@ def signup(request):
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
                 {'template_data': template_data})
+
+@login_required
+def orders(request):
+    template_data = {}
+    template_data['title'] = 'Orders'
+    template_data['orders'] = request.user.order_set.all()
+    return render(request, 'accounts/orders.html',
+        {'template_data': template_data})
 # Create your views here.
