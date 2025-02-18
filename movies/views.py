e254370aeb3d274c5f48cdafc8617d0e95da5edc
ChamonEGT <132160639+ChamonEGT@users.noreply.github.com>
2025-02-18 15:53:44 -0500

    recent changes

--- movies/views.py
+++ movies/views.py
@@ -37,16 +37,21 @@ def create_review(request, id):
         return redirect('movies.show', id=id)
     else:
         return redirect('movies.show', id=id)
+
+
 def index(request):
     search_term = request.GET.get('search')
     if search_term:
         movies = Movie.objects.filter(name__icontains=search_term)
     else:
         movies = Movie.objects.all()
-    template_data = {}
-    template_data['title'] = 'Movies'
-    template_data['movies'] = Movie.objects.all()
-    return render(request, 'movies/index.html',{'template_data': template_data})
+
+    template_data = {
+        'title': 'Movies',
+        'movies': movies  # Use filtered movies
+    }
+
+    return render(request, 'movies/index.html', {'template_data': template_data})
 def show(request, id):
     movie = Movie.objects.get(id=id)
     reviews = Review.objects.filter(movie=movie)
