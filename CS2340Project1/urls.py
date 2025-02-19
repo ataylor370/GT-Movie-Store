6a3d97110c3370ab04e826b8f09e5909e99e8ce4
Jarrett Robinson <mrjarrettrobinson@gmail.com>
2025-02-19 02:15:17 -0500

    ed to do forget password
    :wq

--- CS2340Project1/urls.py
+++ CS2340Project1/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL,
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
     path('orders/', views.orders, name='accounts.orders'),
+
+    path('forgot-password/', views.forgot_password, name='accounts.forgot_password'),
+    path('reset-password/<str:username>', views.reset_password, name='accounts.reset_password'),
+
+
 ]
